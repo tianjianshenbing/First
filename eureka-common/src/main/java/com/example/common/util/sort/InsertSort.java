3c984f7e34b64517074f6475eb886343c8f20f05
tangjikede <tianjianshenbing@126.com>
2020-03-22 17:45:01 +0800

    略微改动一下　　 插入法　注意　交换时下标的变量

---
+++ eureka-common/src/main/java/com/example/common/util/sort/InsertSort.java
@@ -0,0 +1,51 @@
+package com.example.common.util.sort;
+
+import com.example.common.util.RandomCreateArr;
+
+/**
+ * @author : xianzhiyong
+ * @Date: 2020/3/22
+ * @Description : 插入排序
+ *                  扑克牌插牌的方式进行排列
+ *                  无法排序 原因????
+ */
+public class InsertSort {
+
+    public static int[] insertSort(int[] arr){
+
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j>0 && temp< arr[j-1]){
+                arr[j] = arr[j-1];
+                j--;
+            }
+            if(j != i){
+                //此处为最初选的坐标i 的值 和 对比过的 j的值作交换  注意变量的实际位置
+                arr[j] = temp;
+            }
+
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = RandomCreateArr.createArr(5,10);
+
+        //long begin = System.currentTimeMillis();
+
+        int[] res = InsertSort.insertSort(arr);
+
+        for (int a: res) {
+            System.out.print( a + "  ");
+        }
+
+
+        /*long end = System.currentTimeMillis();
+
+        float ext = (float)(end-begin)/1000;
+
+        System.out.println();
+        System.out.println("运行时间： "+ ext);*/
+    }
+}
--- eureka-common/src/main/java/com/example/common/util/sort/SelectSort.java
+++ eureka-common/src/main/java/com/example/common/util/sort/SelectSort.java
@@ -34,16 +34,18 @@ public class SelectSort {
 
     public static void main(String[] args) {
 
+        int[] arr = RandomCreateArr.createArr(5,10);
+
         int[] res = SelectSort.selectSort(arr);
         for (int a: res) {
             System.out.print( a + "   ");
         }
 
        //测试一下随机数组生成工具
-        int[] arr = RandomCreateArr.createArr(10,50);
-        for (int a : arr) {
+       // int[] arr = RandomCreateArr.createArr(10,50);
+       /* for (int a : arr) {
             System.out.print(a + "  ");
-        }
+        }*/
     }
 
 
